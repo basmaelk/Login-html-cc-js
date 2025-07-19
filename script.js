039211c07dad81ce3c2d866032fab13a52f387f3
basmaelk <basma.mendes@gmail.com>
2025-07-19 17:09:44 +0100

    r

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function login() {
+function login(e) {
+  e.preventDefault(); // Corrected spelling
+
   const correctUsername = "basma";
   const correctPassword = "123456";
 
-    const enteredUsername = document.getElementById("username").value;
-    const enteredUsername = document.getElementById("username").value;
-    
-    console.log(enteredUsername);
-    console.log(enteredPassword);
+  const enteredUsername = document.getElementById("username").value;
+  const enteredPassword = document.getElementById("password").value;
 
-    const message = document.getElementById('message');
+  const message = document.getElementById("message");
 
-    if (eneteredUsername === correctUsername && enteredPassword === correctPassword) {
-      message.style.color = "green";
-      message.textContent = "Login successfull";
-    } else {
-      message.style.color = "red"
-      message.textContent = "Invalid username or password.";
-    }
+  if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
+    message.style.color = "green";
+    message.textContent = "Login successful";
+  } else {
+    message.style.color = "red";
+    message.textContent = "Invalid username or password.";
+  }
 }
+
 
